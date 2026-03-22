bfc30e9ef36887af0567d5c266a85d49ca687b0a
Elobari <76933675+Elobari@users.noreply.github.com>
2026-03-22 23:55:16 +0100

    Update transform.py

--- src/transform.py
+++ src/transform.py
@@ -28,7 +28,7 @@ USAGE:
     python src/transform.py                # run standalone
     called automatically by pipeline.py
 
-AUTHOR:     Jonah Knief (i6263747)
+AUTHOR:     Jonah Knief (i6263747) | Artem Vysotskyi (...) | Lyan Eleraky (...) | Loredana Lazari
 COURSE:     Data Engineering and Data Compliance
 UNIVERSITY: Maastricht University
 """
